894409f98c697b85cb0b71a2c71ae056686d38a8
jomarcel <jomarcel_n@outlook.com>
2024-08-10 22:10:18 +1000

    chore: minor code formatting

--- src/api/lotto-picks.ts
+++ src/api/lotto-picks.ts
@@ -1,5 +1,5 @@
 import { ILottoApiBody, LottoResults } from "../types/lotto-picks";
-import axiosConfig from "../axiosConfig";
+import axiosConfig from "../services/axiosConfig";
 
 export const getLottoPicks = async (body: ILottoApiBody): Promise<LottoResults> => {
     const { data } = await axiosConfig.post('/sales/vmax/web/data/lotto/latestresults', body);
